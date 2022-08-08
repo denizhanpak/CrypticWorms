c6a1120c67c158d6139b73956c312a34678dbdbd
denizhanpak <denpak@iu.edu>
2022-08-08 13:18:58 -0400

    Finalized new implementation

--- JuliaModel/cervera_network.jl
+++ JuliaModel/cervera_network.jl
@@ -5,7 +5,7 @@ using Random
 using Graphs
 using DifferentialEquations
 using ChaosTools
-using Basins
+using Printf
 include("./cervera_cells.jl")
 include("./plotting.jl")
 
@@ -51,36 +51,37 @@ fs = []
 ams = []
 Adj = [0 1 ; 1 0]
 G = SimpleDiGraph(Adj)
-gpol_range = range(0,0.11,length=2)
-gmax_range = range(1.1,3.0,length=50)
+gpol_range = range(0.27,0.28,length=20)
+gmax_range = range(1.19,2.0,length=50)
 body = make_body(G)
-gpol_range = [0.11]
+#gpol_range = [0.11]
+gmax_range = [2.0]
 for gpol in gpol_range
     for gmax in gmax_range
         initial = SVector(0.0,0.0)
 
         ds = body_dynamics(body, gmax, gpol)
-        xg = yg = range(-70, 70; length = 100)
+        xg = yg = range(-70.0, 0.0; length = 1000)
         grid = (xg, yg)
         am = AttractorsViaRecurrences(ds, grid)
 
         rng = Random.MersenneTwister(1234)
-        sampler, _ = statespace_sampler(rng; min_bounds=[-70., -70.], max_bounds=[70., 70.])
+        sampler, _ = statespace_sampler(rng; min_bounds=[-70., -70.], max_bounds=[0., 0.])
         push!(fs, basins_fractions(am, sampler; N = 1000, show_progress = false))
         basins, attractors = basins_of_attraction(am)
         if length(attractors) == 2
             tmp = attractors
-            print(attractors[1][1],"\n")
-            print(attractors[2][1],"\n")
             f1 = attractors[1][1][1] - attractors[1][1][2]
             f2 = attractors[2][1][1] - attractors[2][1][2]
             if abs(f1) > abs(f2)
                 normal = 1
+                cryptic = 2
             else
                 normal = 2
+                cryptic = 1
             end
-            a1 = attractors[1][1]
-            a2 = attractors[2][1]
+            a1 = attractors[normal][1]
+            a2 = attractors[cryptic][1]
             ax = [a1[1],a2[1]]
             ay = [a1[2],a2[2]]
         else
@@ -88,11 +89,9 @@ for gpol in gpol_range
             ay = [attractors[1][1][2]]
         end
 
-        n = length(attractors)
-        ids = sort!(unique(basins))
-        fig = plot(heatmap(xg,yg,basins,c=[:red,:blue]))
-        fig = plot(scatter!(ax,ay,c=[:cyan,:magenta]))
+        fig = plot(heatmap(xg,yg,basins,c=[:cyan,:magenta]))
+        fig = plot(scatter!(ay,ax,c=[:blue,:red]))
 
-        savefig(fig,"./phase_diagrams/gmax_$(@sprintf("%.3f", gmax)).png")
+        savefig(fig,"./phase_diagrams/gpol_$(@sprintf("%.4f", gpol)).png")
     end
 end
